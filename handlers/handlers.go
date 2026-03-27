a0a9704719ca8e2a11dc850192e6bd04a74fdf82
Илья Ватютов <vatut007@ya.ru>
2026-03-27 22:18:07 +0300

    first serve

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,15 +1,52 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
 
-func CreateShortLinkHandler(w http.ResponseWriter, r *http.Request) {
+type ShortenRequest struct {
+}
+
+func createShorten(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-Type") != "text/plain" {
+		http.Error(w, "Content type must be text/plain", http.StatusUnsupportedMediaType)
+		return
+	}
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+	domain := r.Host
+	originalURL := string(bodyBytes)
+	w.Header().Set("content-type", "text/plain")
+	url := "http://" + domain + "/" + originalURL + "ffffff"
+	body := strings.ReplaceAll(url, " ", "")
+	fmt.Print(body)
+	w.Write([]byte(body))
+}
+
+func getShorten(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/plain")
+	http.Redirect(w, r, "https://practicum.yandex.ru/", http.StatusTemporaryRedirect)
 
 }
-func GetShortLinkHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://yandex.ru/", http.StatusMovedPermanently)
+
+func ShortLinkHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getShorten(w, r)
+	case http.MethodPost:
+		createShorten(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func RegisterHandlers() {
-	Mux.HandleFunc("/", CreateShortLinkHandler)
-	Mux.HandleFunc("/api", GetShortLinkHandler)
+	Mux.HandleFunc("/", ShortLinkHandler)
 }
