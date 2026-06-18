4a45d7402db59d342017f0f6035c0748a2bb7a8c
Ilya <sov16@ya.ru>
2026-06-18 13:02:16 +0300

    gix test

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_handlers(t *testing.T) {
@@ -26,10 +30,18 @@ func Test_handlers(t *testing.T) {
 		},
 	}
 	for _, test := range tests {
-		t.Run(test.name, func (t *testing.T)  {
+		t.Run(test.name, func(t *testing.T) {
 			request := httptest.NewRequest(http.MethodGet, "/status", nil)
 			w := httptest.NewRecorder()
-			createShorten()
+			createShorten(w, request)
+			res := w.Result()
+			assert.Equal(t, res.StatusCode, test.want.code)
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+
+			require.NoError(t, err)
+			assert.JSONEq(t, string(resBody), test.want.response)
+			assert.Equal(t, res.Header.Get("Content-Type"), test.want.contentType)
 		})
 	}
 }
